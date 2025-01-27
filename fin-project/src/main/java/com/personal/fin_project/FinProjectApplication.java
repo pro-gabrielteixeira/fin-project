e91296ec66300fca404784940c958bc03150782e
Gabriel <pro.gabrielteixeira@gmail.com>
2025-01-27 11:58:25 +0000

    project starter

---
+++ fin-project/src/main/java/com/personal/fin_project/FinProjectApplication.java
@@ -0,0 +1,13 @@
+package com.personal.fin_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinProjectApplication.class, args);
+	}
+
+}
---
+++ fin-project/src/test/java/com/personal/fin_project/FinProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.fin_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
